db3914885acbc841bec7df71f0dfaf2932cdb9bb
DinaYatsuk <133023529+DinaYatsuk@users.noreply.github.com>
2024-02-11 23:50:43 +0700

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -3,7 +3,17 @@ import sys
 import csv
 from multiprocessing import Pool
 
-def read_csv(file_path):
+def read_csv(file_path) -> List[str]:
+    """
+    Read the CSV fle and return a list of text entries.
+    
+    Args:
+    - file_path (str) : Path to the CSV file.
+
+    Returns:
+    - List[str]: List of text entries from the CSV file.
+    """
+    
     data = []
     with open(file_path, newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -13,6 +23,15 @@ def read_csv(file_path):
 
 
 def get_gpt(prompt: str) -> str:
+    """
+    Generate a GPT response based on the provided prompt.
+
+    Args:
+    - prompt (str): The prompt for GPT to generate a response.
+
+    Returns:
+    - str: The generated response from GPT.
+    """
     while True:
         try:
             response = g4f.ChatCompletion.create(
@@ -25,7 +44,18 @@ def get_gpt(prompt: str) -> str:
     print("")
     return response
 
-def process_text(text):
+def process_text(text: str) -> str:
+    
+    """
+    Process a text entry by generating a title using GPT based on the provided text.
+
+    Args:
+    - text (str): The input text to generate a title from.
+
+    Returns:
+    - str: The generated title.
+    """
+    
     cleaned_text = text.replace('"', '\\')
     prompt = (f"""создай заголовок на русском языке, состоящий не более, чем из 10 слов, на основе следующего текста: {cleaned_text}. 
     Выведи только заголовок без каких-либо добавлений, знаков или эмоджи.""")
